586bf2d96dd1911c0e35627d7e5bda20a6ad8b4e
Kyle Thompson <me@kkt.dev>
2024-05-16 11:15:51 -0400

    remove exception before token

--- visivo/commands/init_phase.py
+++ visivo/commands/init_phase.py
@@ -126,7 +126,6 @@ def init_phase():
         Logger.instance().info(
             f"> You may need to register or get added to an account before visiting your profile."
         )
-        raise NotImplementedError("getting token")
         token = click.prompt("? Personal token", type=str)
         raise NotImplementedError("got token")
         os.makedirs(f"{user_home}/.visivo", exist_ok=True)
